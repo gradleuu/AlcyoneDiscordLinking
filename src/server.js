77b5fbc0a7dbf1dcbce1b95136dd100c1c126879
Glitch (linked-role-discord-bot) <none>
2024-11-10 09:07:48 +0000

    🎍🍐 Checkpoint ./src/server.js:71702610/8955

--- src/server.js
+++ src/server.js
@@ -1,41 +1,46 @@
-import express from 'express';
-import cookieParser from 'cookie-parser';
+import express from "express";
+import cookieParser from "cookie-parser";
 
-import config from './config.js';
-import * as discord from './discord.js';
-import * as storage from './storage.js';
+import config from "./config.js";
+import * as discord from "./discord.js";
+import * as storage from "./storage.js";
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://poster:<db_password>@somnacreare.in65n.mongodb.net/?retryWrites=true&w=majority&appName=Somnacreare";
-
+import mongoose from "mongoose";
+import mongodb from "mongodb";
 
 /**
  * Main HTTP server used for the bot.
  */
 
- const app = express();
- app.use(cookieParser(config.COOKIE_SECRET));
+const app = express();
 
- /**
-  * Just a happy little route to show our server is up.
-  */
- app.get('/', (req, res) => {
-   res.send('👋');
- });
+// Connect to MongoDB
+const dbUri =
+  "mongodb+srv://poster:J5r3xlMyJ5k36U9t@somnacreare.in65n.mongodb.net/?retryWrites=true&w=majority&appName=Somnacreare";
+mongoose.connect(dbUri);
+
+app.use(cookieParser(config.COOKIE_SECRET));
+
+/**
+ * Just a happy little route to show our server is up.
+ */
+app.get("/", (req, res) => {
+  res.send("👋");
+});
 
 /**
  * Route configured in the Discord developer console which facilitates the
- * connection between Discord and any additional services you may use. 
- * To start the flow, generate the OAuth2 consent dialog url for Discord, 
+ * connection between Discord and any additional services you may use.
+ * To start the flow, generate the OAuth2 consent dialog url for Discord,
  * and redirect the user there.
  */
-app.get('/linked-role', async (req, res) => {
+app.get("/linked-role", async (req, res) => {
   const { url, state } = discord.getOAuthUrl();
 
   // Store the signed state param in the user's cookies so we can verify
   // the value later. See:
   // https://discord.com/developers/docs/topics/oauth2#state-and-security
-  res.cookie('clientState', state, { maxAge: 1000 * 60 * 5, signed: true });
+  res.cookie("clientState", state, { maxAge: 1000 * 60 * 5, signed: true });
 
   // Send the user to the Discord owned OAuth2 authorization endpoint
   res.redirect(url);
@@ -50,16 +55,16 @@ app.get('/linked-role', async (req, res) => {
  * 3. Stores the OAuth2 Discord Tokens in Redis / Firestore
  * 4. Lets the user know it's all good and to go back to Discord
  */
- app.get('/discord-oauth-callback', async (req, res) => {
+app.get("/discord-oauth-callback", async (req, res) => {
   try {
     // 1. Uses the code and state to acquire Discord OAuth2 tokens
-    const code = req.query['code'];
-    const discordState = req.query['state'];
+    const code = req.query["code"];
+    const discordState = req.query["state"];
 
     // make sure the state parameter exists
     const { clientState } = req.signedCookies;
     if (clientState !== discordState) {
-      console.error('State verification failed.');
+      console.error("State verification failed.");
       return res.sendStatus(403);
     }
 
@@ -73,14 +78,14 @@ app.get('/linked-role', async (req, res) => {
       refresh_token: tokens.refresh_token,
       expires_at: Date.now() + tokens.expires_in * 1000,
     });
-    
+
     console.log(tokens);
     console.log(userId);
 
     // 3. Update the users metadata, assuming future updates will be posted to the `/update-metadata` endpoint
     await updateMetadata(userId);
 
-    res.send('You can go back to Discord now');
+    res.send("You can go back to Discord now");
   } catch (e) {
     console.error(e);
     res.sendStatus(500);
@@ -88,14 +93,14 @@ app.get('/linked-role', async (req, res) => {
 });
 
 /**
- * Example route that would be invoked when an external data source changes. 
+ * Example route that would be invoked when an external data source changes.
  * This example calls a common `updateMetadata` method that pushes static
  * data to Discord.
  */
- app.post('/update-metadata', async (req, res) => {
+app.post("/update-metadata", async (req, res) => {
   try {
     const userId = req.body.userId;
-    await updateMetadata(userId)
+    await updateMetadata(userId);
 
     res.sendStatus(204);
   } catch (e) {
@@ -104,14 +109,13 @@ app.get('/linked-role', async (req, res) => {
 });
 
 /**
- * Given a Discord UserId, push static make-believe data to the Discord 
- * metadata endpoint. 
+ * Given a Discord UserId, push static make-believe data to the Discord
+ * metadata endpoint.
  */
 async function updateMetadata(userId) {
-
   // Fetch the Discord tokens from storage
   const tokens = await storage.getDiscordTokens(userId);
-    
+
   let metadata = {};
   try {
     // Fetch or generate metadata
@@ -125,8 +129,7 @@ async function updateMetadata(userId) {
     e.message = `Error fetching external data: ${e.message}`;
     console.error(e);
   }
-  
-  
+
   await discord.pushMetadata(userId, tokens, metadata);
 }
 
